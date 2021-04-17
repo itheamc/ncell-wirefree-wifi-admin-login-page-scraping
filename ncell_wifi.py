ebfdfc2ed2450f8059e28ece5533c1b4f271fcee
Amit Chaudhary <41687974+itheamc@users.noreply.github.com>
2021-04-17 17:43:42 +0545

    initial commit
    This is an python web scrapping script to automatically login into the ncell wirefree wifi admin panel.

---
+++ ncell_wifi.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+import time
+
+# specify the chrome driver path to use
+driver = webdriver.Chrome('C:/Users/User/Downloads/chromedriver_win32/chromedriver.exe')
+driver.get('http://192.168.0.1/')
+
+# targeting the login button
+time.sleep(1.5)
+login_btn = WebDriverWait(driver, 10).until(ec.element_to_be_clickable((By.ID, 'loginlink')))
+login_btn.click()
+
+# target username
+username = WebDriverWait(driver, 10).until(ec.element_to_be_clickable((By.CSS_SELECTOR, "input[name='txtUsr']")))
+password = WebDriverWait(driver, 10).until(ec.element_to_be_clickable((By.CSS_SELECTOR, "input[name='txtPwd']")))
+
+# enter username and password
+time.sleep(2)
+username.clear()
+username.send_keys("admin")     # Enter Your username here
+password.clear()
+password.send_keys("admin")     # Enter your password here
+
+# target the login button and click it
+time.sleep(1.5)
+button = WebDriverWait(driver, 5).until(ec.element_to_be_clickable((By.ID, 'btnLogin')))
+button.click()
+
+# targeting to setting div to see the data usage
+time.sleep(1.5)
+div = WebDriverWait(driver, 10).until(ec.element_to_be_clickable((By.CSS_SELECTOR, "a[data-trans='device_setting']")))
+div.click()
+
+driver.maximize_window()
